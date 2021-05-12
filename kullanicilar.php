5208ed9e03abbf65f089f51d4b3571ae4d7a7293
eekilinc <74473092+eekilinc@users.noreply.github.com>
2021-05-12 17:59:03 +0300

    tabloduzenleme

--- kullanicilar.php
+++ kullanicilar.php
@@ -19,10 +19,7 @@ $kayitsayisi = $sorgu->rowCount();
 
         <div id="content">
             <div class="container-fluid">
-                <div class="d-sm-flex justify-content-between align-items-center mb-4">
-                    <h3 class="text-dark mb-0">Kulanıcılar</h3>
-                    <a href="kullanici.php?islem=ekle">Yeni Kullanıcı Ekle</a>
-                </div>
+
 
                 <?php
                 if (isset($_SESSION['durum'])) {
@@ -34,58 +31,71 @@ $kayitsayisi = $sorgu->rowCount();
                 }
 
                 ?>
-                <div class="table-responsive table mt-2" id="dataTable" role="grid"
-                     aria-describedby="dataTable_info">
-                    <?php if ($kayitsayisi > 0) {
-                    ?>
-                    <table class="table table-bordered table-striped table-hover" id="kullaniciTablo">
-                        <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">İd</th>
-                            <th scope="col">Ad</th>
-                            <th scope="col">Soyad</th>
-                            <th scope="col">E-Posta</th>
-                            <th scope="col" class="text-center">Aktif Mi</th>
-                            <th class="text-center">İşlemler</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach ($kullanicilar as $satir) { ?>
-                            <tr>
-                                <td><?php echo $satir['id'] ?></td>
-                                <td><?php echo $satir['Ad'] ?></td>
-                                <td><?php echo $satir['Soyad'] ?></td>
-                                <td><?php echo $satir['Eposta'] ?></td>
-                                <td class="text-center"><span
-                                            class=" <?php echo $satir['Aktif'] == 0 ? 'fa fa-times-circle' : 'fa fa-check-circle'; ?>"></span>
-                                </td>
-                                <td class="text-center">
-                                    <a href="kullanici.php?islem=duzenle&id=<?php echo $satir['id'] ?>">
-                                        <span class="fa fa-edit"></span></a> |
-                                    <a href="kullanici.php?islem=sil&id=<?php echo $satir['id'] ?>">
-                                        <span class="fa fa-remove"></span></a>
-                                </td>
-                            </tr>
-                        <?php } ?>
 
-                        </tbody>
-                        <tfoot class="thead-dark">
-                        <tr>
-                            <th>İd</th>
-                            <th>Ad</th>
-                            <th>Soyad</th>
-                            <th>E-Posta</th>
-                            <th class="text-center">Aktif Mi</th>
-                            <th class="text-center">İşlemler</th>
-                        </tr>
-                        </tfoot>
-                    </table>
+                <div class="container-fluid">
+                    <div class="card shadow">
+                        <div class="card-header py-3">
+                            <h5 class="text-primary m-0 font-weight-bold">Kullanıcılar</h5>
+                            <a class="badge badge-info" href="kullanici.php?islem=ekle">Yeni Kullanıcı Ekle</a>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive table mt-2" id="dataTable" role="grid"
+                                 aria-describedby="dataTable_info">
+                                <?php if ($kayitsayisi > 0) {
+                                    ?>
+                                    <table class="table table-bordered table-striped table-hover" id="kullaniciTablo">
+                                        <thead class="thead-dark">
+                                        <tr>
+                                            <th scope="col">İd</th>
+                                            <th scope="col">Ad</th>
+                                            <th scope="col">Soyad</th>
+                                            <th scope="col">E-Posta</th>
+                                            <th scope="col" class="text-center">Aktif Mi</th>
+                                            <th class="text-center">İşlemler</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <?php foreach ($kullanicilar as $satir) { ?>
+                                            <tr>
+                                                <td><?php echo $satir['id'] ?></td>
+                                                <td><?php echo $satir['Ad'] ?></td>
+                                                <td><?php echo $satir['Soyad'] ?></td>
+                                                <td><?php echo $satir['Eposta'] ?></td>
+                                                <td class="text-center"><span
+                                                            class=" <?php echo $satir['Aktif'] == 0 ? 'fa fa-times-circle' : 'fa fa-check-circle'; ?>"></span>
+                                                </td>
+                                                <td class="text-center">
+                                                    <a href="kullanici.php?islem=duzenle&id=<?php echo $satir['id'] ?>">
+                                                        <span class="fa fa-edit"></span></a> |
+                                                    <a href="kullanici.php?islem=sil&id=<?php echo $satir['id'] ?>">
+                                                        <span class="fa fa-remove"></span></a>
+                                                </td>
+                                            </tr>
+                                        <?php } ?>
+
+                                        </tbody>
+                                        <tfoot class="thead-dark">
+                                        <tr>
+                                            <th>İd</th>
+                                            <th>Ad</th>
+                                            <th>Soyad</th>
+                                            <th>E-Posta</th>
+                                            <th class="text-center">Aktif Mi</th>
+                                            <th class="text-center">İşlemler</th>
+                                        </tr>
+                                        </tfoot>
+                                    </table>
+
+                                    <?php
+                                } else
+                                    echo "Gösterilecek kayıt bulunamadı";
+                                ?>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            <?php
-            }
-            else
-                echo "Gösterilecek kayıt bulunamadı";
-            ?>
+
 
             </div>
         </div>
